cdb4556006e85ed525f6f070ceae2b3ce4b769b9
root <root@djdevops-app-acs-frontend-bootstrap-jx9sg>
2024-10-11 13:29:38 +0000

    Initial commit

---
+++ src/reportWebVitals.js
@@ -0,0 +1,33 @@
+import {ConsoleInstrumentation, getWebInstrumentations, initializeFaro} from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+var _faro = null;
+export function initFaroOTEL() {
+  _faro = initializeFaro({
+    url: `${process.env.REACT_APP_FARO_URL}`,
+    apiKey: `${process.env.REACT_APP_FARO_APIKEY}`,
+    trackWebVitalsAttribution: true,
+    instrumentations: [...getWebInstrumentations(), new TracingInstrumentation(), new ConsoleInstrumentation()],
+    app: {
+      name: `djdevops-app-frontend`,
+      version: '1.0.0',
+    },
+  });
+}
+
+export const faro = _faro;
+
+export default reportWebVitals;
+
